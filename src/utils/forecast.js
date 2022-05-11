0e4ee9d8a0e0deaeedcc77827da83458a39cfb42
Leonardo Jorge <lpj.jorge@gmail.com>
2022-05-11 15:08:04 -0300

    Changes to forecast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain, with a wind speed of ' + body.current.wind_speed + ' Km/h.')
         }
     })
 }

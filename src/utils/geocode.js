af63625c2d3e13c2d2f7014f0e38af759a16698d
Leonardo Jorge <lpj.jorge@gmail.com>
2022-05-11 12:55:56 -0300

    Init Commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic3VkZGVuMSIsImEiOiJjbDFwbmgyanExZjd1M2RtbXc0cXR0N3U3In0.HdRpkTJErC7S0glrMof_yw&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
